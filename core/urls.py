a2ee7e886ddba9b879e0be2afbcb042141caa2a8
Coca-Mann <juliano.ostroski@gmail.com>
2025-05-26 22:09:25 -0400

    feat: Autoredirect to admin when URL is requested

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=True), name='index_redirect'),
     path('admin/', admin.site.urls),
 ]
 

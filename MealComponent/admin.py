6e6528fb3b47c4a08ee1c5fe606c88fc58e85be8
Coca-Mann <juliano.ostroski@gmail.com>
2025-05-26 20:42:42 -0400

    feat: Created admin for 'MealComponent' app

--- MealComponent/admin.py
+++ MealComponent/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import MealComponent, Ingredient
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+
+@admin.register(MealComponent)
+class MealComponentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'component_type')
+    list_filter = ('component_type',)
+    search_fields = ('name', 'description')
+    inlines = [IngredientInline]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('description', 'meal_component')
+    search_fields = ('description',)
